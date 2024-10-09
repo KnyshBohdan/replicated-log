4c5dedd75406fcffbfab28789ef43b8b368d4e80
KnyshBohdan <knysh.bohdan0205@gmail.com>
2024-10-09 22:16:19 +0300

    Added grpc support

--- master/src/main/java/com/replog/master/controller/MessageController.java
+++ master/src/main/java/com/replog/master/controller/MessageController.java
@@ -1,8 +1,13 @@
 package com.replog.master.controller;
 
 import com.replog.master.model.Message;
+import com.replog.proto.MessageProto;
+import com.replog.proto.MessageServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
@@ -15,12 +20,17 @@ public class MessageController {
 
     private List<Message> messages = new ArrayList<>();
 
+    @Value("${secondary.host}")
+    private String secondaryHost;
+
+    @Value("${secondary.port}")
+    private int secondaryPort;
+
     @PostMapping("/messages")
     public ResponseEntity<String> addMessage(@RequestBody Message message) {
         logger.info("Received POST request with message: {}", message.getContent());
         messages.add(message);
 
-        // Simulate replication to secondaries and wait for ACKs
         replicateToSecondaries(message);
 
         return ResponseEntity.ok("Message received and replicated");
@@ -33,19 +43,32 @@ public class MessageController {
     }
 
     private void replicateToSecondaries(Message message) {
-        // TODO: Implement the actual replication logic
-
         logger.info("Replicating message to secondaries: {}", message.getContent());
 
-        // Simulate delay to test blocking replication
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(secondaryHost, secondaryPort)
+                .usePlaintext()
+                .build();
+
         try {
-            Thread.sleep(1000); // Simulate delay (1 second)
-        } catch (InterruptedException e) {
-            logger.error("Error during replication delay", e);
-            Thread.currentThread().interrupt();
+            MessageServiceGrpc.MessageServiceBlockingStub stub = MessageServiceGrpc.newBlockingStub(channel);
+
+            MessageProto.Message grpcMessage = MessageProto.Message.newBuilder()
+                    .setContent(message.getContent())
+                    .build();
+
+            MessageProto.Ack ack = stub.replicateMessage(grpcMessage);
+
+            if (ack.getSuccess()) {
+                logger.info("Received ACK from secondary");
+            } else {
+                logger.warn("Secondary failed to replicate message");
+            }
+        } catch (Exception e) {
+            logger.error("Error during replication to secondary", e);
+        } finally {
+            channel.shutdown();
         }
 
-        // Simulate receiving ACKs from secondaries
-        logger.info("Received ACKs from all secondaries");
+        logger.info("Replication to secondaries completed");
     }
 }
